4fd49803b4b22a2e8deb30ab6bc7b5888aa43aad
Afonso Rocha <afonsoandrerocha@gmail.com>
2019-05-25 22:29:18 -0300

    circle resolved commit

--- lib/circle.rb
+++ lib/circle.rb
@@ -4,11 +4,14 @@ class Circle < GeometricBase
   attr_writer :radius
 
   def initialize(radius)
+    @r = radius
   end
 
   def area
+    Math::PI * @r * @r
   end
 
   def perimeter
+    Math::PI * 2.0 * @r
   end
 end
